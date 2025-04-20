0983f1ae2fcaaffc458c07be11c9a90a25a251e6
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-04-20 22:39:14 +0200

    push

--- data/kaggle.py
+++ data/kaggle.py
@@ -1,13 +1,6 @@
 import kagglehub
 
-# Download the latest version.
-kagglehub.dataset_download('bricevergnou/spotify-recommendation')
+# Download latest version
+path = kagglehub.dataset_download("jerryqu/reddit-conversations")
 
-# Download a specific version.
-kagglehub.dataset_download('bricevergnou/spotify-recommendation/versions/1')
-
-# Download a single file.
-kagglehub.dataset_download('bricevergnou/spotify-recommendation', path='data.csv')
-
-# Download a dataset or file, even if previously downloaded to cache.
-kagglehub.dataset_download('bricevergnou/spotify-recommendation', force_download=True)
+print("Path to dataset files:", path)
