7ecdefb5d598ccef0081ddc3753b744fe0c1980c
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-04-23 15:58:14 +0200

    push

--- model/transformer.py
+++ model/transformer.py
@@ -1,7 +1,7 @@
+from __future__ import annotations
 import math
 import inspect
 from dataclasses import dataclass
-from __future__ import annotations
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
