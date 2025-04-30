f0768a67f14c37aa301e45641faccf39958da9da
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-04-30 19:00:09 +0200

    push

---
+++ model/gpt.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import math
+
+class GPTConfig:
+    def __init__(self, vocab_size, block_size, n_layer=4, n_head=4, n_embd=128):
+        self.vocab_size = vocab_size
+        self.block_size = block_size
+        self.n_layer = n_layer
+        self.n_head = n_head
+        self.n_embd = n_embd
+
+class GPT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(config.vocab_size, config.n_embd)
+        self.position_embedding_table = nn.Embedding(config.block_size, config.n_embd)
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
+
+        self.block_size = config.block_size
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+        token_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=idx.device))
+        x = token_emb + pos_emb
+        logits = self.lm_head(x)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = nn.functional.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -self.block_size:]
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :]
+            probs = nn.functional.softmax(logits, dim=-1)
+            next_token = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, next_token), dim=1)
+        return idx
