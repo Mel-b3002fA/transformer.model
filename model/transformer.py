fa83c3bbee0f813f217fb6e115eb5d74c4050df5
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-05-03 22:37:18 +0200

    ”push”

--- model/transformer.py
+++ model/transformer.py
@@ -205,7 +205,7 @@ class GPT(nn.Module):
         config_args['vocab_size'] = 50257 
         config_args['block_size'] = 1024 
         config_args['bias'] = True 
-        # we can override the dropout rate, if desired
+  
         if 'dropout' in override_args:
             print(f"overriding dropout rate to {override_args['dropout']}")
             config_args['dropout'] = override_args['dropout']
