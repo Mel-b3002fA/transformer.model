050e3c5e76a384d631f713c88c565dc588f5b8f1
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-05-03 22:37:23 +0200

    ”push”

--- model/transformer.py
+++ model/transformer.py
@@ -209,7 +209,7 @@ class GPT(nn.Module):
         if 'dropout' in override_args:
             print(f"overriding dropout rate to {override_args['dropout']}")
             config_args['dropout'] = override_args['dropout']
-        # create a from-scratch initialized minGPT model
+
         config = GPTConfig(**config_args)
         model = GPT(config)
         sd = model.state_dict()
