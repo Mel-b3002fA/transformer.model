6411ad4dbff5f7cde9d0a11a8acd92d56fa22f0a
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-05-04 22:55:25 +0200

    ”push”

--- plot_loss.py
+++ plot_loss.py
@@ -1,21 +1,17 @@
 import matplotlib.pyplot as plt
 import json
 
-# Load losses from the JSON file
 with open("out/losses.json", "r") as f:
     losses = json.load(f)
 
-# Plot the loss curve
+
 plt.plot(losses)
 plt.xlabel('Iterations')
 plt.ylabel('Loss')
 plt.title('Training Loss Curve')
 
-# Save the plot as a PNG file
 plt.savefig('out/real_losscurve.png')
 
-# Optionally, show the plot (uncomment if you want to display the plot)
-# plt.show()
 
 print("✅ Loss curve saved as out/real_losscurve.png")
 
